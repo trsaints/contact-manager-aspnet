b01ffbfb301a78dd48fb70f337752ee5971c787d
trsaints <thiago.dev2k3@gmail.com>
2024-06-14 13:08:25 -0300

    feat: create constants for role management operations

---
+++ Authorization/ContactOperations.cs
@@ -0,0 +1,25 @@
+namespace ContactManager.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+public static class ContactOperations
+{
+    public static OperationAuthorizationRequirement Create = new() { Name=Constants.CreateOperationName };
+    public static OperationAuthorizationRequirement Read = new () { Name=Constants.ReadOperationName };
+    public static OperationAuthorizationRequirement Update = new () { Name=Constants.UpdateOperationName };
+    public static OperationAuthorizationRequirement Delete = new ()  { Name=Constants.DeleteOperationName };
+    public static OperationAuthorizationRequirement Approve = new () { Name=Constants.ApproveOperationName };
+    public static OperationAuthorizationRequirement Reject = new () { Name=Constants.RejectOperationName };
+}
+
+public class Constants
+{
+    public static readonly string CreateOperationName = "Create";
+    public static readonly string ReadOperationName = "Read";
+    public static readonly string UpdateOperationName = "Update";
+    public static readonly string DeleteOperationName = "Delete";
+    public static readonly string ApproveOperationName = "Approve";
+    public static readonly string RejectOperationName = "Reject";
+
+    public static readonly string ContactAdministratorsRole = "ContactAdministrators";
+    public static readonly string ContactManagersRole = "ContactManagers";
+}
